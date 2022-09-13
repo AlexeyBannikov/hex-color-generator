6364694326f4e35207e82263d89a9cd6444e0be5
Aleksey Bannikov <bannikoff.alesha@yandex.ru>
2022-09-13 13:59:24 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const changeBtn = document.getElementById('changeBtn');
+const header2 = document.getElementById('header2');
+const bodyVar = document.body;
+
+const getHex = () => {
+  let result = [];
+  let hexRef = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+
+  for (let i = 0; i < 6; i++) {
+    result.push(hexRef[Math.floor(Math.random() * 16)]);
+  }
+
+  return result.join('');
+};
+
+changeBtn.addEventListener('click', () => {
+  let firstColor = getHex();
+  let secondColor = getHex();
+  let styleString = `background: linear-gradient(to right, #${firstColor}, #${secondColor})`;
+
+  bodyVar.style = styleString;
+
+  header2.innerHTML = styleString;
+});
